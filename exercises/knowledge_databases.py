6d08a873c06d8f82ba3b7ed38a73f5d83d430ad2
alice19-meet <alice19@meet.mit.edu>
2018-08-06 14:08:15 +0300

    done

--- exercises/knowledge_databases.py
+++ exercises/knowledge_databases.py
@@ -8,20 +8,30 @@ Base.metadata.create_all(engine)
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-def add_article():
-	pass
+def add_article(name,title, rating):
+    article_object = Knowledge(
+        name=name,
+        title=title,
+        rating=rating)
+    session.add(article_object)
+    session.commit()
+
+
+add_article("Noam", "music", 9)
+add_article("Rakan", "chemistry", 8)
+add_article("Leen", "architecture", 7)
 
 def query_all_articles():
-	pass
+    pass
 
 def query_article_by_topic():
-	pass
+    pass
 
 def delete_article_by_topic():
-	pass
+    pass
 
 def delete_all_articles():
-	pass
+    pass
 
 def edit_article_rating():
-	pass
+    pass
--- exercises/knowledge_model.py
+++ exercises/knowledge_model.py
@@ -6,12 +6,28 @@ from sqlalchemy import create_engine
 Base = declarative_base()
 
 class Knowledge(Base):
-	# Create a table with 4 columns
-	# The first column will be the primary key
-	# The second column should be a string representing
-	# the name of the Wiki article that you're referencing
-	# The third column will be a string representing the 
-	# topic of the article. The last column will be
-	# an integer, representing your rating of the article.
+    # Create a table with 4 columns
+    # The first column will be the primary key
+    # The second column should be a string representing
+    # the name of the Wiki article that you're referencing
+    # The third column will be a string representing the 
+    # topic of the article. The last column will be
+    # an integer, representing your rating of the article.
 
-	pass
+    __tablename__ = 'knowledge'
+    site_id = Column(Integer, primary_key=True)
+    name = Column(String)
+    title= Column(String)
+    rating = Column(Integer)
+
+    def __repr__(self):
+        return ("If you want to learn about {}, you should look at the Wikipedia article called {}. We gave this article a rating of {} out of 10!".format(
+                     self.name, self.title,  self.rating))
+
+a=Knowledge(name="Noam", title="music", rating=9)
+b=Knowledge(name="Rakan", title="chemistry", rating=8)
+c=Knowledge(name="Leen", title="architechture", rating=7)
+
+print(a)
+print(b)
+print(c)
