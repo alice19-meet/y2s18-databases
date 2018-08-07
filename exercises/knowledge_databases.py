6cf15a7a1a5e802686d5d351caebf9e92312c068
alice19-meet <alice19@meet.mit.edu>
2018-08-07 12:14:00 +0300

    databases

--- exercises/knowledge_databases.py
+++ exercises/knowledge_databases.py
@@ -17,21 +17,64 @@ def add_article(name,title, rating):
     session.commit()
 
 
-add_article("Noam", "music", 9)
-add_article("Rakan", "chemistry", 8)
-add_article("Leen", "architecture", 7)
+# add_article("Noam", "music", 9)
+# add_article("Rakan", "chemistry", 8)
+# add_article("Leen", "architecture", 7)
 
 def query_all_articles():
-    pass
+    article=session.query(
+        Knowledge).all()
+    return article
 
-def query_article_by_topic():
-    pass
+#print (query_all_articles()) 
 
-def delete_article_by_topic():
-    pass
+def query_article_by_topic(their_name):
+    article=session.query(
+        Knowledge).filter_by(
+        name=their_name).first() 
+    return article
 
-def delete_all_articles():
-    pass
+#print(query_article_by_topic("Noam"))
 
-def edit_article_rating():
-    pass
+def query_article_by_rating(threshold):
+    rating=threshold
+    knowledge= query_all_articles()
+    rated_list=[]
+    for article in knowledge:
+        if article.rating<threshold:
+            rated_list.append(article)
+    return rated_list
+#print(query_article_by_rating(8))
+
+def query_article_by_primary_key(primary_key):
+    article=session.query(
+        Knowledge).filter_by(
+            site_id=primary_key).first()
+    return article
+
+#print(query_article_by_primary_key(3))
+
+# def delete_article_by_topic(topic):
+#     session.query(Knowledge).filter_by(
+#         title=topic).delete()
+#     session.commit()
+
+# def delete_all_articles():
+#     session.query(Knowledge).delete()
+#     session.commit()
+
+
+def edit_article_rating(updated_rating, article_title):
+    article = session.query(
+        Knowledge).filter_by(
+        title=article_title).all()
+    for i in range(len(article)):
+        article[i].rating=updated_rating
+    session.commit()
+
+
+edit_article_rating(10, "music")
+
+    
+
+print(query_all_articles())
